307ff96b13c13d6f9e7a59fc879fddf1ec3a41ba
Mhamed Ben Miled <madbmd@gmail.com>
2020-04-16 12:00:36 -0400

    Ajoutez des fichiers projet.

---
+++ EmuleRQ3270/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace EmuleRQ3270
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStripContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(1229, 793);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.LeftToolStripPanelVisible = false;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.RightToolStripPanelVisible = false;
+            this.toolStripContainer1.Size = new System.Drawing.Size(1229, 818);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1229, 818);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+    }
+}
+
---
+++ EmuleRQ3270/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace EmuleRQ3270
+{
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser chromiumWebBrowser;
+        public GlobalKeyboardHook gHook;
+        public Form1()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeChromium();
+
+            //Start listening keybord events
+            gHook = new GlobalKeyboardHook();
+            gHook.KeyDown += new KeyEventHandler(gHook_KeyDown);
+            foreach (Keys key in Enum.GetValues(typeof(Keys)))
+            {
+                gHook.HookedKeys.Add(key);
+            }
+            gHook.hook();
+        }
+
+        private void InitializeChromium()
+        {
+            //this.KeyPreview = true;
+            //this.KeyPress += new KeyPressEventHandler(Form1_KeyPress);
+            // Autoriser Celsharp à utiliser les ressources locales
+            
+            //chromiumWebBrowser.BrowserSettings = browserSettings;
+            //Cef.Initialize(browserSettings);
+
+            String page = string.Format(@"{0}\index.htm", Application.StartupPath);
+            if (!File.Exists(page))
+            {
+                MessageBox.Show("Erreur, la page WEB est inéxistante : " + page);
+            }
+            chromiumWebBrowser = new ChromiumWebBrowser(page);
+            BrowserSettings browserSettings = new BrowserSettings();
+            browserSettings.FileAccessFromFileUrls = CefState.Enabled;
+            browserSettings.UniversalAccessFromFileUrls = CefState.Enabled;
+            chromiumWebBrowser.BrowserSettings = browserSettings;
+            toolStripContainer1.ContentPanel.Controls.Add(chromiumWebBrowser);
+            //this.Controls.Add(chromiumWebBrowser);
+
+            
+
+            chromiumWebBrowser.Dock = DockStyle.Fill;
+        }
+
+        void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine("Bouton pressé: " + e.KeyChar.ToString());
+            if (e.KeyChar >= 112 && e.KeyChar <= 123)
+            {
+                //MessageBox.Show("Bouton pressé: " + e.KeyChar.ToString());
+                //browser.ExecuteScriptAsyncWhenPageLoaded("doEnter();");
+            }
+        }
+
+        public void gHook_KeyDown(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine("Bouton pressé: " + e.KeyData.ToString());
+            if (ApplicationFocus.ApplicationIsActivated())
+            {
+                if (e.KeyData.ToString().ToUpper().IndexOf("Shift".ToUpper()) >= 0
+                   && e.KeyValue == 66)//B = 66
+                {
+                    //ALT + B 
+                    new Thread(() =>
+                    {
+                        // execute this on the gui thread. (winforms)
+                        this.Invoke(new Action(delegate
+                        {
+                            //tosBrowserBtnBack_Click(this, new EventArgs());
+                            chromiumWebBrowser.ExecuteScriptAsyncWhenPageLoaded("doEnter();");
+                        }));
+
+                    }).Start();
+                }
+                else if (e.KeyData.ToString().ToUpper().IndexOf("Return".ToUpper()) >= 0)//Enter
+                {
+                    //ALT + F
+                    new Thread(() =>
+                    {
+                        // execute this on the gui thread. (winforms)
+                        this.Invoke(new Action(delegate
+                        {
+                            //tosBrowserBtnFor_Click(this, new EventArgs());
+                            chromiumWebBrowser.ExecuteScriptAsyncWhenPageLoaded("doF1();");
+                        }));
+
+                    }).Start();
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+            gHook.unhook();
+        }
+
+
+        
+    }
+}
